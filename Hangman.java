ad679cc2225aac8fecdff328dac13e521ef89e17
Sahar Jalili <153284020+SaharJalili236@users.noreply.github.com>
2026-05-13 16:54:33 +0200

    Refactor Hangman game output messages

--- Hangman.java
+++ Hangman.java
@@ -4,23 +4,21 @@ import java.util.ArrayList;
 
 public class Hangman {
     public static void main(String[] args) {
-        // لیست کلمات vordefinierten Liste [cite: 6]
         String[] woerter = {"entwickler", "java", "programm", "computer", "hangman"};
         Random random = new Random();
-        String dasWort = woerter[random.nextInt(woerter.length)]; // Wortauswahl [cite: 6]
+        String dasWort = woerter[random.nextInt(woerter.length)];
         
         char[] erratenesWort = new char[dasWort.length()];
         for (int i = 0; i < erratenesWort.length; i++) {
-            erratenesWort[i] = '_'; // Anzeigen با زیرخط 
+            erratenesWort[i] = '_';
         }
 
-        int versuche = 6; // تعداد Versuche [cite: 24]
-        ArrayList<Character> gerateneBuchstaben = new ArrayList<>(); // لیست حروف [cite: 14]
+        int versuche = 6;
+        ArrayList<Character> gerateneBuchstaben = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Spiel: Hangman");
 
-        // Spielverlauf [cite: 15]
         while (versuche > 0 && String.valueOf(erratenesWort).contains("_")) {
             System.out.println("\nWort: " + String.valueOf(erratenesWort));
             System.out.println("Verbleibende Versuche: " + versuche);
@@ -29,7 +27,6 @@ public class Hangman {
             
             String input = scanner.next().toLowerCase();
 
-            // Überprüfe die Gültigkeit (فقط یک حرف) [cite: 8]
             if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
                 System.out.println("Ungültige Eingabe!");
                 continue;
@@ -37,7 +34,6 @@ public class Hangman {
 
             char buchstabe = input.charAt(0);
 
-            // جلوگیری از حروف تکراری [cite: 9]
             if (gerateneBuchstaben.contains(buchstabe)) {
                 System.out.println("Bereits geraten!");
                 continue;
@@ -45,7 +41,6 @@ public class Hangman {
 
             gerateneBuchstaben.add(buchstabe);
 
-            // چک کردن وجود حرف در کلمه [cite: 16, 17]
             if (dasWort.indexOf(buchstabe) >= 0) {
                 for (int i = 0; i < dasWort.length(); i++) {
                     if (dasWort.charAt(i) == buchstabe) {
@@ -53,15 +48,14 @@ public class Hangman {
                     }
                 }
             } else {
-                versuche--; // کاهش فرصت [cite: 17]
+                versuche--;
             }
         }
 
-        // بررسی برنده یا بازنده بودن [cite: 18, 19, 20]
         if (!String.valueOf(erratenesWort).contains("_")) {
-            System.out.println("\nHerzlichen Glückwunsch! Sie haben das Wort erraten: " + dasWort);
+            System.out.println("\nHerzlichen Glückwunsch! Wort: " + dasWort);
         } else {
-            System.out.println("\nSpiel verloren. Das Wort war: " + dasWort);
+            System.out.println("\nVerloren! Das Wort war: " + dasWort);
         }
         scanner.close();
     }
